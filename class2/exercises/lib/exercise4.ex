c2582280c1084355b0d3e45b9dba701eacae1d65
LKlemens <klemens.lukaszczyk@gmail.com>
2024-04-10 19:04:22 +0200

    Update exercise4.ex

---
+++ class2/exercises/lib/exercise4.ex
@@ -0,0 +1,11 @@
+defmodule Exercises.Exercise4 do
+  @doc """
+   Spawn a new process, register it under :hello name, 
+   wait for :ping message and send a :timeout msg to :test process after 500ms.
+   input: none
+   returns: pid
+  """
+  def send_timeout() do
+    # write your code here
+  end
+end
