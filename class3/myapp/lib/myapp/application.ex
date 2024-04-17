38813254d083fe37cc35e9b6a84555377fa8f97b
Paweł Długosz <pawel.dlugosz@erlang-solutions.com>
2024-04-17 13:49:44 +0200

    Exercise 5

--- class3/myapp/lib/myapp/application.ex
+++ class3/myapp/lib/myapp/application.ex
@@ -5,7 +5,12 @@ defmodule MyApp.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      StackSupervisor
+      StackSupervisor,
+      # Starts a supervisor by calling: MyApp.Supervisor.start_link([])
+      # Note: this will break some of the previous tests which start
+      #       a process explicitly because then GenServer.start_link/3
+      #       returns {:error, {:already_started, pid}}
+      MyApp.Supervisor
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
